80989daadf0fac548c876cf593c89ac91619a1e2
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-18 06:39:29 +0100

    Set up app module for code-first approach
    The application wasn’t working because I’d forgotten to import the config for GraphQL to use a code-first approach.
    
    In this commit also adds the auto schema file to gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthorModule],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }),
   providers: [AppService],
 })
 export class AppModule {}
