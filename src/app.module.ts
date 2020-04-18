d44862fc6a700940b9896d646b73ab76a1b9a57e
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-18 06:40:42 +0100

    Install Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [GraphQLModule.forRoot({
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
   }),
+  MongooseModule.forRoot('mongodb://graphql-playlist:test123@ds259410.mlab.com:59410/books', { useNewUrlParser: true, useUnifiedTopology: true }),
+  AuthorModule
+],
   providers: [AppService],
 })
 export class AppModule {}
