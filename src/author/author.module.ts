2ba1e03e4180fdd60041bc37b1db18c7d8d37c34
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-20 06:54:14 +0100

    Import schema into module

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { AuthorResolver } from './author.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthorSchema } from './author.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Author', schema: AuthorSchema}])],
   providers: [AuthorService, AuthorResolver]
 })
 export class AuthorModule {}
