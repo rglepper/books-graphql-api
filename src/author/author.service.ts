64eeefa8205e8be593c042a6e6711ed7f6d11216
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-20 06:57:13 +0100

    Get authors from database

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -8,23 +8,8 @@ export class AuthorService {
 
 	constructor(@InjectModel('Author') private readonly authorModel: Model<Author>)  {}
 
-	async getAuthors() {
-		return [
-			{
-			  "name": "John Kennedy Toole",
-			  "age": 32,
-			  "id": "5e5f89565b2f756a6d27b6c9"
-			},
-			{
-			  "name": "Gabriel Garcia Marquez",
-			  "age": 88,
-			  "id": "5e5f89565b2f756a6d27b6ca"
-			},
-			{
-			  "name": "Eduardo Mendoza",
-			  "age": 78,
-			  "id": "5e5f89565b2f756a6d27b6cb"
-			}
-		  ];
+
+	async getAuthors(): Promise<Author[]> {
+		return await this.authorModel.find().exec();
 	}
 }
