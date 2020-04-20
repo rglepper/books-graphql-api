12bbc1f58085f69807ebd107424d6568ee6b341f
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-20 06:56:29 +0100

    Inject Author Model into service

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Author } from './author.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthorService {
 
+	constructor(@InjectModel('Author') private readonly authorModel: Model<Author>)  {}
 
 	async getAuthors() {
 		return [
