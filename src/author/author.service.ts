bb3e7d03feb8301d7d3c966d8ea35ee569274b75
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-20 10:30:56 +0100

    Save new author to database

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Author } from './author.interface';
 import { Model } from 'mongoose';
+import { AuthorInput } from './author.input';
 
 @Injectable()
 export class AuthorService {
@@ -11,5 +12,11 @@ export class AuthorService {
 
 	async getAuthors(): Promise<Author[]> {
 		return await this.authorModel.find().exec();
+	}
+
+	async addAuthor(newAuthor: AuthorInput): Promise<Author> {
+		const author = new this.authorModel(newAuthor);
+
+		return await author.save();
 	}
 }
