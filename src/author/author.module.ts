f8a2a28ee92d8912e6560ebab6b196486eb057ed
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-23 16:35:41 +0100

    Remove book model from module

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -4,13 +4,12 @@ import { AuthorResolver } from './author.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorSchema } from './author.schema';
 import { BookService } from '../book/book.service';
-import { BookModule } from '../book/book.module';
 import { BookModel } from '../book/book.model';
-import { BookSchema } from '../book/book.schema';
+import { BookModule } from '../book/book.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{name: 'Author', schema: AuthorSchema},{name: 'Book', schema: BookSchema}]),
+    MongooseModule.forFeature([{name: 'Author', schema: AuthorSchema}]),
     BookModule
   ],
   providers: [AuthorService, AuthorResolver, BookService, BookModel]
