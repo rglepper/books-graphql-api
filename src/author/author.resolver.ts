b37d59360ccbd33807b7918bf400fa687ce1d1a3
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-23 16:52:47 +0100

    Reformat author resolver

--- src/author/author.resolver.ts
+++ src/author/author.resolver.ts
@@ -5,35 +5,39 @@ import { CreateAuthorInput } from './create-author.input';
 import { Author } from './author.interface';
 import { UpdateAuthorInput } from './update-author.input';
 import { BookService } from '../book/book.service';
+import { BookModel } from '../book/book.model';
 
 
-@Resolver(() => AuthorModel)
+@Resolver(of => AuthorModel)
 export class AuthorResolver {
-	constructor(
-		private readonly authorService: AuthorService,
-		private readonly bookService: BookService) {}
+  constructor(
+	private readonly authorService: AuthorService,
+	private readonly bookService: BookService
+  ) {}
 
-	@Query(() => [AuthorModel])
-	authors() {
-		return this.authorService.getAuthors();
-	}
+  @Query(() => [AuthorModel], {name: 'authors'})
+  async getAuthors() {
+    return await this.authorService.getAuthors();
+  }
 
-	@Mutation(() => AuthorModel)
-	async createAuthor(@Args('authorData') authorData: CreateAuthorInput): Promise<Author> {
-		return this.authorService.addAuthor(authorData)
-	}
+  @Mutation(() => AuthorModel)
+  async createAuthor(
+    @Args('authorData') authorData: CreateAuthorInput,
+  ): Promise<Author> {
+    return this.authorService.addAuthor(authorData);
+  }
 
-	@Mutation(() => AuthorModel)
-	async updateAuthor(
-		@Args('id') id:string,
-		@Args('changes') changes:UpdateAuthorInput,
-	): Promise<Author> {
-		return await this.authorService.updateAuthor(id,changes);
-	}
+  @Mutation(() => AuthorModel)
+  async updateAuthor(
+    @Args('id') id: string,
+    @Args('changes') changes: UpdateAuthorInput,
+  ): Promise<Author> {
+    return await this.authorService.updateAuthor(id, changes);
+  }
 
-	@ResolveField()
-	async books(@Parent() author) {
-		const { id } = author;
-		return await this.bookService.findAllByAuthor(id);
-	}
+  @ResolveField('books', returns => [BookModel])
+  async books(@Parent() author: AuthorModel) {
+    const { id } = author;
+    return await this.bookService.findAllByAuthor(id);
+  }
 }
